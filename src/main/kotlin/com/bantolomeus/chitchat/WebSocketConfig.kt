5ba82d172e284edeccbce12b99a790ff0c6846cf
chrb <chrb@spreadshirt.net>
2017-08-26 21:30:59 +0200

    #12 removed unnecessary config method and created run script for serving front end stuff to spring

--- src/main/kotlin/com/bantolomeus/chitchat/WebSocketConfig.kt
+++ src/main/kotlin/com/bantolomeus/chitchat/WebSocketConfig.kt
@@ -1,15 +1,13 @@
 package com.bantolomeus.chitchat
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 
+
 @Configuration
-@EnableWebMvc
 @EnableWebSocket
 class WebSocketConfig: WebMvcConfigurerAdapter(), WebSocketConfigurer {
 
@@ -17,7 +15,4 @@ class WebSocketConfig: WebMvcConfigurerAdapter(), WebSocketConfigurer {
         registry?.addHandler(EchoWebSocketHandler(), "/ws")?.setAllowedOrigins("*")
     }
 
-    override fun configureDefaultServletHandling(configurer: DefaultServletHandlerConfigurer?) {
-        configurer?.enable()
-    }
 }
